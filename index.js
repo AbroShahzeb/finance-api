878f69e6ab6be6877f973adea81a28fd5ba40869
AbroShahzeb <khizr772@gmail.com>
2025-04-24 00:32:00 +0500

    Huhu

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! hehe");
 });
 
 app.listen(3008, () => {
-  console.log("Server is running on port 3008");
+  console.log("Server is running on port 3008 ");
 });
